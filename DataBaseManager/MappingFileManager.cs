773f89c39c7ed8f4ed79f4f6ad800e2be505c4a1
akgjohansson <johansson.mcquack@gmail.com>
2017-09-14 17:03:41 +0200

    Automatiserar uppbyggnad av databas mha nHibernate (schemaexport)

---
+++ DataBaseManager/MappingFileManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseManager
+{
+    internal class MappingFileManager
+    {
+        public string BeginningOfFile { get; set; }
+        public string EndOfFile { get; set; }
+
+        public MappingFileManager()
+        {
+            Assembly.LoadFile("MappingFileBuilders\\beginningOfFile.txt");
+            //BeginningOfFile = 
+            EndOfFile = $"    }}{Environment.NewLine}}};";
+        }
+        void AddOneToMany()
+        {
+
+        }
+
+        void AddManyToMany()
+        {
+
+        }
+
+        
+    }
+}
