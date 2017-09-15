652073c34dddca7a4bd4e98d779650eada99ce81
akgjohansson <johansson.mcquack@gmail.com>
2017-09-15 11:53:01 +0200

    Automation av mapper fortsatt arbete

---
+++ DataBaseManager/MapperTextParts.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseManager
+{
+    public class MapperTextParts
+    {
+
+        public string EndOfMapper { get; set; }
+        public MapperTextParts()
+        {
+            EndOfMapper = $"            }});{Environment.NewLine}";
+        }
+        public string BeginningOfMapper(string className) {
+            return $"            _modelMapper.Class<{className}>(e =>            {{{Environment.NewLine}";
+        }
+
+        
+
+        public string Id(string className) {
+            return $"                e.Id(p => p.{className}Id, p => p.Generator(Generators.GuidComb));{Environment.NewLine}";
+        }
+        
+        public string Properties(string[] properties)
+        {
+            string output = "";
+            foreach (string property in properties)
+            {
+                output += $"                e.Property(p => p.{property});{Environment.NewLine}";
+            }
+            return output;
+        }
+
+        public string OneToMany(string tableName, string foreignTableName)
+        {
+            return $"                e.Set(p => p.{foreignTableName}, p =>{Environment.NewLine}" +
+                $"                {{{Environment.NewLine}" +
+                $"                    p.Inverse(true);{Environment.NewLine}" +
+                $"                    p.Cascade(Cascade.All);{Environment.NewLine}" +
+                $"                    p.Key(k => k.Column(col => col.Name(\"{tableName}Id\")));{Environment.NewLine}" +
+                $"                }}, p => p.OneToMany());{Environment.NewLine}";
+        }
+
+        public string ManyToOne(string tableName, string foreignTableName)
+        {
+            return $"                e.ManyToOne(p => p.{foreignTableName}, mapper =>{Environment.NewLine}" +
+                $"               {{{Environment.NewLine}" +
+                $"                   mapper.Column(\"{foreignTableName}Id\");{Environment.NewLine}" +
+                $"                   mapper.NotNullable(true);{Environment.NewLine}" +
+                $"                   mapper.Cascade(Cascade.None);{Environment.NewLine}" +
+                $"               }});";
+        }
+        public string ManyToMany(string tableName, string foreignTableName , bool firstToMention)
+        {
+            string inverseText = "";
+            if (firstToMention)
+                inverseText = $"                    collectionMapping.Inverse(true);{Environment.NewLine}";
+
+            return $"                e.Set( x => x.Customers  , collectionMapping =>{Environment.NewLine}" +
+                $"                {{{Environment.NewLine}" +
+                $"                    collectionMapping.Table(\"{tableName}{foreignTableName}\");{Environment.NewLine}" +
+                $"                    collectionMapping.Cascade(Cascade.None);{Environment.NewLine}" +
+                $"                    collectionMapping.Key(keyMap => keyMap.Column(\"{tableName}Id\"));{Environment.NewLine}" +
+                $"                }}, map => map.ManyToMany(p => {{{Environment.NewLine}" +
+                $"                    p.Column(\"{foreignTableName}Id\");{Environment.NewLine}" +
+                $"                    p.ForeignKey(\"FK_{foreignTableName}{tableName}_{foreignTableName}\");" +
+                $"                    p.Class(typeof({foreignTableName}));{Environment.NewLine}" +
+                $"                }}));{Environment.NewLine}";
+        }
+
+    }
+}
--- DataBaseManager/MappingFileManager.cs
+++ DataBaseManager/MappingFileManager.cs
@@ -4,30 +4,173 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace DataBaseManager
 {
-    internal class MappingFileManager
+    public class MappingFileManager
     {
-        public string BeginningOfFile { get; set; }
-        public string EndOfFile { get; set; }
+        private string BeginningOfFile { get; set; }
+        private string EndOfFile { get; set; }
+        private string FullPathToHere { get; set; }
+        private string FullPathToMFB { get; set; }
+        private MapperTextParts mapperText { get; set; }
 
         public MappingFileManager()
         {
-            Assembly.LoadFile("MappingFileBuilders\\beginningOfFile.txt");
-            //BeginningOfFile = 
+            FullPathToHere = GetPathToHomeDir();
+            FullPathToMFB = FullPathToHere + "\\MappingFileBuilders";
+            BeginningOfFile = System.IO.File.ReadAllText($"{FullPathToHere}\\MappingFileBuilders\\beginningOfFile.txt");
             EndOfFile = $"    }}{Environment.NewLine}}};";
+            mapperText = new MapperTextParts();
         }
-        void AddOneToMany()
+
+        public void NewTable(string tableName, string[] properties)
+        {
+            string firstPart = "";
+            firstPart += mapperText.BeginningOfMapper(tableName);
+            firstPart += mapperText.Id(tableName);
+            firstPart += mapperText.Properties(properties);
+
+            string secondPart = "";
+            secondPart += mapperText.EndOfMapper;
+            SaveBothParts(firstPart, secondPart, tableName);
+        }
+
+
+        public void AddOneToMany(string tableName , string foreignTableName)
+        {
+            string secondPart1 = GetSecondPart(tableName);
+            secondPart1 = mapperText.OneToMany(tableName, foreignTableName) + secondPart1;
+
+            SaveSecondPart(secondPart1 , tableName);
+
+            string secondPart2 = GetSecondPart(foreignTableName);
+            secondPart2 = mapperText.ManyToOne(tableName, foreignTableName) + secondPart2;
+            SaveSecondPart(secondPart2, foreignTableName);
+        }
+        public void AddManyToMany(string tableName , string foreignTableName)
+        {
+            string secondPart1 = GetSecondPart(tableName);
+            secondPart1 = mapperText.ManyToMany(tableName, foreignTableName, true) +  secondPart1;
+            SaveSecondPart(secondPart1, tableName);
+
+            string secondPart2 = GetSecondPart(foreignTableName);
+            secondPart2 = mapperText.ManyToMany(tableName, foreignTableName, false) + secondPart2;
+            SaveSecondPart(secondPart2, foreignTableName);
+        }
+
+        private void SaveBothParts(string firstPart, string secondPart , string tableName)
+        {
+            SaveFirstPart(firstPart, tableName);
+            SaveSecondPart(secondPart, tableName);
+        }
+
+        private void SaveFirstPart(string firstPart , string tableName)
+        {
+            System.IO.File.WriteAllText($"{FullPathToMFB}\\table{tableName}.beginning.txt", firstPart);
+        }
+
+        public void SaveSecondPart(string secondPart , string tableName)
+        {
+            System.IO.File.WriteAllText($"{FullPathToMFB}\\table{tableName}.end.txt", secondPart);
+        }
+
+        private string GetFirstPart(string tableName)
+        {
+            string filePath = $"{FullPathToMFB}\\table{tableName}.beginning.txt";
+            if (File.Exists(filePath))
+            {
+                return System.IO.File.ReadAllText(filePath);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private string GetSecondPart(string tableName)
+        {
+            string filePath = $"{FullPathToMFB}\\table{tableName}.end.txt";
+            if (File.Exists(filePath))
+            {
+                return System.IO.File.ReadAllText(filePath);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        ///  Removes all tables
+        /// </summary>
+        public void TabulaRasa()
+        {
+            string[] files = GetAllTables();
+            string[] tableNames = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                tableNames[i] = files[i].Substring(6, files[i].Length - 3);
+            }
+        }
+
+        public string[] GetAllTables()
+        {
+            string[] allFiles = System.IO.Directory.GetFiles(FullPathToHere);
+            string[] files = Validator.Selections.ReturnMatches(allFiles, "^tables*", false);
+            string[] tables = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                tables[i] = files[i].Split('.')[0];
+            }
+            return Validator.Selections.RemoveDuplicates(tables);
+        }
+
+        /// <summary>
+        /// Builds the Mapping file
+        /// </summary>
+        internal void Build()
         {
 
         }
 
-        void AddManyToMany()
+        /// <summary>
+        /// Adds newly created class file to project (in the .csproj file)
+        /// </summary>
+        /// <param name="fileName"></param>
+        internal void AddFileToProject(string fileName)
         {
+            string pathToCsprojFile = "DataBaseManager\\DataBaseManager.csproj";
 
         }
 
-        
+
+        private static string GetPathToHomeDir()
+        {
+            string fullPathHere = System.IO.Path.GetFullPath(".");
+            int safetyCounter = 0;
+            while (!System.IO.Directory.Exists($"{fullPathHere}\\DataBaseManager"))
+            {
+                fullPathHere = System.IO.Path.GetFullPath($"{fullPathHere}\\..");
+                if (safetyCounter++ > 10)
+                {
+                    throw new Exception("Directory not found!");
+                }
+            }
+            safetyCounter = 0;
+            while (!System.IO.Directory.Exists($"{fullPathHere}\\MappingFileBuilders"))
+            {
+                fullPathHere += "\\DataBaseManager";
+                if (safetyCounter++ > 10)
+                {
+                    throw new Exception("Directory MappingFileBuilders does not exist!");
+                }
+            }
+
+            return fullPathHere;
+        }
     }
 }
